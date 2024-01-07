04dd5db084993be972cdbe57b1d0810f78b68650
chenjiayi <ffchenjiayi@chinatelecom.cn>
2024-01-07 16:12:46 +0800

    feat:gulimall-product 代码生成

---
+++ gulimall-common/src/test/java/com/atguigu/guilimall/common/GuilimallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/SkuSaleAttrValueDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.product.dao;
+
+import com.atguigu.gulimall.product.entity.SkuSaleAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku销售属性&值
+ * 
+ * @author cjy
+ * @email cjy@qq.com
+ * @date 2023-12-20 23:44:22
+ */
+@Mapper
+public interface SkuSaleAttrValueDao extends BaseMapper<SkuSaleAttrValueEntity> {
+	
+}
