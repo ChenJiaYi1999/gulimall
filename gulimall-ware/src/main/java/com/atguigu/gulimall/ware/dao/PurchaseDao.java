a4ae97843f2f2c9d144a13c34b69742feebed976
chenjiayi <ffchenjiayi@chinatelecom.cn>
2024-01-08 15:24:58 +0800

    feat:ware库存管理模块代码生成

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/PurchaseDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ware.dao;
+
+import com.atguigu.gulimall.ware.entity.PurchaseEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 采购信息
+ * 
+ * @author cjy
+ * @email cjy@qq.com
+ * @date 2024-01-08 15:10:58
+ */
+@Mapper
+public interface PurchaseDao extends BaseMapper<PurchaseEntity> {
+	
+}
---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareSkuDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ware.dao;
+
+import com.atguigu.gulimall.ware.entity.WareSkuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品库存
+ * 
+ * @author cjy
+ * @email cjy@qq.com
+ * @date 2024-01-08 15:10:58
+ */
+@Mapper
+public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
+	
+}
